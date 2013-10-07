69092ed82b77c5be236de206fefeb25b30af68e9
Francisco J. Lopez-Pellicer <fjlopez@unizar.es>
2013-10-07 21:16:02 +0200

    Big WS examples (src-gen code included!)

---
+++ bigws/hellocli/src-gen/main/java/iw7i/bigws/hellows/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://hellows.bigws.iw7i/")
+package iw7i.bigws.hellows;
--- bigws/hellows/src/main/java/iw7i/bigws/hellows/HelloWebService.java
+++ bigws/hellows/src/main/java/iw7i/bigws/hellows/HelloWebService.java
@@ -6,7 +6,7 @@ import javax.jws.WebService;
 @WebService
 public class HelloWebService {
 	
-	@WebMethod
+	@WebMethod()
 	public String sayHello(String name) {
 		return "Hello "+name;
 	}
