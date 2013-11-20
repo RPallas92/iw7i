29d3e8f3f720828998703d56f6d15de061ba6463
Francisco J. Lopez-Pellicer <fjlopez@unizar.es>
2013-11-20 12:23:57 +0100

    Fix bugs

--- messaging/chat/src/main/java/iw7i/messages/chat/ChatEndpoint.java
+++ messaging/chat/src/main/java/iw7i/messages/chat/ChatEndpoint.java
@@ -6,24 +6,34 @@ import java.util.logging.Logger;
 
 import javax.websocket.EncodeException;
 import javax.websocket.OnClose;
+import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint(value = "/chat/{room}", encoders = ChatMessageEncoder.class, decoders = ChatMessageDecoder.class)
 public class ChatEndpoint {
 	private final Logger log = Logger.getLogger(getClass().getName());
+	@OnError
+	public void error(final Session session, final Throwable err) {
+		log.log(Level.SEVERE, "fallo crítico", err);
+	}
+
+	
 	@OnOpen
 	public void open(final Session session) {
 	}
 
 	@OnClose
 	public void onClose(final Session session) {
+		log.info("close");
 	}
 
 	@OnMessage
 	public void onMessage(final Session session, final ChatMessage chatMessage) {
+		log.info("message "+chatMessage);
 		try {
 				session.getBasicRemote().sendObject(chatMessage);
 		} catch (IOException | EncodeException e) {
